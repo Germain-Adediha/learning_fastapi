279442c4dfd95baecbeafaffcd863cf36ea0f5d6
Germain <adedihagermain@gmail.com>
2022-03-10 17:27:06 +0300

    Learned Nested models

--- nested_models.py
+++ nested_models.py
@@ -1,16 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, HttpUrl
 from typing import Optional, List
 
+
 app = FastAPI()
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
     name : str
     description:  Optional[str] = None
-    price : float
+    price : Optional[float] = Query(None ,gt=0,lt=10000)
     tax:Optional[float] = None
-    tags :list = []
+    tags :list[str] = []
+    tags2 : set[str] = set()
+    image: Optional[List[Image]] = None # pydantic models can be also used as subtypes of list , set  etc...
+
+
+class Offer(BaseModel):
+    name:str
+    description: Optional[str] = None
+    price: float
+    item:List[Item]
+
+
 
 
 @app.put("/items/{item_id}")
@@ -23,3 +39,18 @@ async def update_item(
         "Item": item
     }
     return result
+
+
+@app.post("/offers/")
+async def create_offer(offer : Offer):
+    return offer
+
+
+@app.post("/images/multiples/")
+async def create_multiple_images(images : list[Image]):
+    return images
+
+
+@app.post("/index-weights/")
+async def create_index_weights(weights:dict[int,float]):
+    return weights
