4f00c678ef7b6401813fa5c67815a60e169096e1
Germain <adedihagermain@gmail.com>
2022-03-31 00:59:45 +0300

    working with databases(postgre)

---
+++ sql_app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+db_url = "postgresql://germain:17mawuko17@localhost/my_db"
+
+engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base =declarative_base()
